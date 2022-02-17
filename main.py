5ac9a0212009fe304aee8977c12612af48fe4791
nbasara <nbasara@ucsc.edu>
2022-02-16 23:30:08 -0800

    autograder progressively unzips the directories moves file into working directory correctly grades outputs score cleans directory

--- main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("does this return 0?" + 0)
-
-if __name__ == '__main__':
-    main()
--- python_test.sh
+++ python_test.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-score=0
-python3 main.py  < input.txt > output.txt 2>&1
+for s in *.zip; do
+    # copy the necessary files into working directory
+    #students score for demo
+    score=0
+    #parse to get student name
+    readarray -d ' - ' -t stdArr <<< "$s"
+    studentName="${stdArr[2]}"
+    unzip -qq "$s"
+    student=${s::-4}
+    cp "$student"/ArrayList.py .
+    cp "$student"/ArrayQueue.py .
+    cp "$student"/BookStore.py .
+    #run program on input from input.txt, with output + errors being
+    #written to output.txt
+    python3 main.py  < input.txt > output.txt 2>&1 
+    #check if last command exited with error in program
+    if [ $? -eq 0 ]; then
+        ((score=score+1))
+        #run the autograder for the students output
+        python3 AutoGrader.py 
+        if [ $? -eq 0 ]; then
+            ((score=score+1))
+        fi
+        mv reason.txt "$student"
+        echo "${studentName} ${score}"
+    else
+        # change this to output the student name and their grade of  0
+        echo "${studentName} 0"
+        #move their error to student directory for saving for later
+        mv output.txt "$student"/reason.txt
+    fi
 
-if [ $? -eq 0 ]; then
-    ((score=score+1))
-else
-	echo "We finished with and error!"
-    exit 0
-fi
+    #change this to be student name and grade
 
-echo $score
+    # rm student files to do again
+    rm ArrayList.py 
+    rm ArrayQueue.py 
+    rm BookStore.py 
+done
