f3103f2f6e8dd60b5fa78e40960388c1319a9241
nbasara <nbasara@ucsc.edu>
2022-02-17 00:42:59 -0800

    Made it so auto grader only accepts a zip files without folders Properly creates a folder with students names

--- python_test.sh
+++ python_test.sh
@@ -5,13 +5,15 @@ for s in *.zip; do
     #students score for demo
     score=0
     #parse to get student name
-    readarray -d ' - ' -t stdArr <<< "$s"
+    readarray -d '- ' -t stdArr <<< "$s"
     studentName="${stdArr[2]}"
-    unzip -qq "$s"
+    studentName="$(echo -e "${studentName}" | tr -d '[:space:]')"
+    echo $studentName
+    unzip -qq "$s" -d $studentName
     student=${s::-4}
-    cp "$student"/ArrayList.py .
-    cp "$student"/ArrayQueue.py .
-    cp "$student"/BookStore.py .
+    cp "$studentName"/ArrayList.py .
+    cp "$studentName"/ArrayQueue.py .
+    cp "$studentName"/BookStore.py .
     #run program on input from input.txt, with output + errors being
     #written to output.txt
     python3 main.py  < input.txt > output.txt 2>&1 
@@ -19,17 +21,17 @@ for s in *.zip; do
     if [ $? -eq 0 ]; then
         ((score=score+1))
         #run the autograder for the students output
-        python3 AutoGrader.py 
+        python3 OutputGrader.py 
         if [ $? -eq 0 ]; then
             ((score=score+1))
         fi
-        mv reason.txt "$student"
+        mv reason.txt "$studentName"
         echo "${studentName} ${score}"
     else
         # change this to output the student name and their grade of  0
         echo "${studentName} 0"
         #move their error to student directory for saving for later
-        mv output.txt "$student"/reason.txt
+        mv output.txt "$studentName"/reason.txt
     fi
 
     #change this to be student name and grade
